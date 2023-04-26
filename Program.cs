c50e9e1e156369188e08d9a42dfd375d493bd281
VSANJEKA <VSANJEKA@INLEN8520076178>
2023-04-26 15:13:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AnonymousMethodDemo
+{
+    class Program
+    {
+        public delegate void sum(int x, int y);
+        static void Main(string[] args)
+        {
+            sum s = delegate (int x, int y)
+            {
+                Console.WriteLine("Inside Anonymous method");
+                Console.WriteLine("Sum = {0}", x + y);
+            };
+            s(7, 12);
+        }
+    }
+}
+
